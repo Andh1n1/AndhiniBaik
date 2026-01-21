9d4911ff650745a89fad3e371d6743a48ab6d5c5
Andh1n1 <andhini.aja09@gmail.com>
2026-01-21 09:50:16 +0700

    upload php1

---
+++ belajarphp/hello.php
@@ -0,0 +1,10 @@
+<?php
+ echo "<a href=hello.php style=color:cadetblue ; >Home</a> |
+        <a href=contact.php style=color: blue; >Contact</a>";
+ echo "<h1>Biodata Saya</h1>";
+ echo "<img src=andhini-cantik.jpg width=200 height=150 style=border-radius: 50%;/>";
+ echo "<p>Nama  : Andhini Putri Wijaya</p>";
+ echo "<p>Kelas : XI TJKT 3</p>";
+ echo "<p>TTL   : Bandung, 16 Februari 2009</p>";
+ echo "<p>Hobi  : Menyanyi, Makeup, Membuat Bolu xixix</p>";
+?>
