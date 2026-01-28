9f11f5418badb314f042d45c9e7cda9f787bfbe0
Andh1n1 <andhini.aja09@gmail.com>
2026-01-28 09:29:57 +0700

    update1

--- belajarphp/hello.php
+++ belajarphp/hello.php
@@ -1,10 +1,29 @@
 <?php
- echo "<a href=hello.php style=color:cadetblue ; >Home</a> |
-        <a href=contact.php style=color: blue; >Contact</a>";
- echo "<h1>Biodata Saya</h1>";
- echo "<img src=andhini-cantik.jpg width=200 height=150 style=border-radius: 50%;/>";
- echo "<p>Nama  : Andhini Putri Wijaya</p>";
- echo "<p>Kelas : XI TJKT 3</p>";
- echo "<p>TTL   : Bandung, 16 Februari 2009</p>";
- echo "<p>Hobi  : Menyanyi, Makeup, Membuat Bolu xixix</p>";
+echo "<a href=hello.php style=color:cadetblue ; >Home</a>";
+echo "<h1>Biodata Saya</h1>";
+$namaLengkap = 'Andhini Putri Wijaya';
+$jenisK = 'Perempuan';
+$umur = '16';
+$berat = '44';
+$tinggi = '149';
+$kelas = 'XI TJKT 3';
+$TTL = 'Bandung, 16 Februari 2009';
+
+ echo "<img src=andhini-cantik.jpg width=200 height=150 style=border-radius: 50%;/> <br>";
+ echo "<br>Nama  : $namaLengkap<br>";
+ echo "Kelas : $kelas<br>";
+ echo "TTL   : $TTL<br>";
+ echo "Jenis Kelamin  : $jenisK<br>";
+ echo "Umur : $umur<br>";
+ echo "Berat Badan : $berat kg <br>";
+ echo "Tinggi : $tinggi cm<br>";
+ 
+
+$a = '5';
+$b = '2';
+$c = $a + $b;
+// penjumlahan
+
+echo "$a + $b = $c";
+echo "<hr>";
 ?>
