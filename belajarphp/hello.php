bb89f015966b64aea37d6705e0c0b5f8bf86f6b0
Andh1n1 <andhini.aja09@gmail.com>
2026-01-30 15:14:34 +0700

    update2

--- belajarphp/hello.php
+++ belajarphp/hello.php
@@ -17,13 +17,37 @@ $TTL = 'Bandung, 16 Februari 2009';
  echo "Umur : $umur<br>";
  echo "Berat Badan : $berat kg <br>";
  echo "Tinggi : $tinggi cm<br>";
- 
 
-$a = '5';
-$b = '2';
+ echo "<br>Jika kamu belanja lebih dari 100rb maka anda akan dapat hadiah<br>";
+ $total_belanja = 10000000;
+if($total_belanja > 100000){
+    echo "Anda belanja 150rb<br>";
+    echo "Anda dapat hadiah!<br>";
+    } else {
+    echo "Anda belanja kurang dari 100rb<br>";
+    echo "Belanja Lebih banyak yups!";
+}
+
+
+$a = 5;
+$b = 2;
 $c = $a + $b;
 // penjumlahan
 
 echo "$a + $b = $c";
 echo "<hr>";
+
+$speed = 83;
+$speed *= 10;
+$speed += 1;
+$speed -= 800;
+echo "$speed<br> <hr>";
+
+$suka = true;
+
+// menggunakan operator ternary
+$jawab = $suka ? "yups": "no";
+
+// menampilkan jawaban
+echo "kamu suka kucing? $jawab";
 ?>
