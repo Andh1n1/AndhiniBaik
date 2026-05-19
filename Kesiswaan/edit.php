d70dbda65f7e0c4bb5bfda0f561efc010f1e75ca
Andh1n1 <andhini.aja09@gmail.com>
2026-05-19 17:43:42 +0700

    CRUD 3

--- Kesiswaan/edit.php
+++ Kesiswaan/edit.php
@@ -1,31 +1,39 @@
 <?php
+// Pastikan session_start() sudah aktif di koneksi.php atau tambahkan di sini jika belum
 include 'koneksi.php';
 
 $target_id = $_GET['id'];
 
 // Kunci Pintu: Jika dia Guru, pastikan dia hanya buka ID miliknya sendiri
-if ($_SESSION['role'] == 'guru' && $target_id != $_SESSION['id']) {
+if (isset($_SESSION['role']) && $_SESSION['role'] == 'guru' && $target_id != $_SESSION['id']) {
     die("Anda tidak berhak mengedit data orang lain!");
 }
 
-// Ambil data lama
-$query = mysqli_query($koneksi, "SELECT users.username, biodata_guru.* FROM users JOIN biodata_guru ON users.id = biodata_guru.user_id WHERE users.id = '$target_id'");
-$data = mysqli_fetch_array($query);
+// Ambil data lama menggunakan JOIN yang benar
+$query1 = mysqli_query($koneksi, "SELECT username from users");
+$query2 = mysqli_query($koneksi, "SELECT * from pengurus");
+$data1 = mysqli_fetch_array($query1);
+$data2 = mysqli_fetch_array($query2);
+
+// Jika ID tidak ditemukan di database, hentikan proses agar tidak error
+if (!$data2) {
+    die("Data dengan ID tersebut tidak ditemukan di database! Periksa kembali tabel users dan pengurus Anda.");
+}
 
 // Proses jika tombol update ditekan
 if (isset($_POST['update'])) {
     $nama_lengkap   = $_POST['nama_lengkap'];
-    $NIP        = $_POST['NIP'];
-    $jabatan  = $_POST['jabatan'];
-    $bidang = $_POST['bidang'];
-    $masa_jabatan    = $_POST['masa_jabatan'];
-    $username_baru = $_POST['username'];
-    $password_baru = $_POST['password'];
+    $NIP            = $_POST['NIP'];
+    $jabatan        = $_POST['jabatan'];
+    $bidang         = $_POST['bidang'];
+    $masa_jabatan   = $_POST['masa_jabatan'];
+    $username_baru  = $_POST['username'];
+    $password_baru  = $_POST['password'];
 
     // Update biodata
-    mysqli_query($koneksi, "UPDATE biodata_guru SET nama_lengkap='$nama_lengkap', NIP='$NIP', jabatan='$jabatan', bidang='$bidang', masa_jabatan='masa_jabatan' WHERE user_id='$target_id'");
+    mysqli_query($koneksi, "UPDATE pengurus SET nama_lengkap='$nama_lengkap', NIP='$NIP', jabatan='$jabatan', bidang='$bidang', masa_jabatan='$masa_jabatan' WHERE user_id='$target_id'");
 
-    // Update username (Karena input username readonly untuk guru, nilainya tetap sama)
+    // Update username
     mysqli_query($koneksi, "UPDATE users SET username='$username_baru' WHERE id='$target_id'");
 
     // Update password HANYA jika kolomnya diisi
@@ -35,23 +43,28 @@ if (isset($_POST['update'])) {
     }
 
     header("location:pengurus.php");
+    exit;
 }
 ?>
 
 <!DOCTYPE html>
 <html>
+<head>
+    <title>Edit Data</title>
+</head>
 <body>
     <h2>Edit Data</h2>
     <form method="POST" action="">
         <h4>Akun Login</h4>
-        Username: <input type="text" name="username" value="<?php echo $data['username']; ?>" <?php echo ($_SESSION['role'] == 'guru') ? 'readonly' : ''; ?> required><br>
+        Username: <input type="text" name="username" value="<?= $data1['username']; ?>" <?= (isset($_SESSION['role']) && $_SESSION['role'] == 'guru') ? 'readonly' : ''; ?> required><br><br>
         Password Baru: <input type="password" name="password" placeholder="Kosongkan jika tidak ganti"><br>
         
         <h4>Biodata Lengkap</h4>
-        Nama Lengkap: <input type="text" name="nama" value="<?php echo $data['nama_lengkap']; ?>" required><br>
-        NIP: <textarea name="alamat" required><?php echo $data['alamat']; ?></textarea><br>
-        Jabatan: <input type="text" name="tempat_lahir" value="<?php echo $data['tempat_lahir']; ?>" required><br>
-        Bidang: <input type="date" name="tanggal_lahir" value="<?php echo $data['tanggal_lahir']; ?>" required><br><br>
+        Nama Lengkap: <input type="text" name="nama_lengkap" value="<?php echo $data2['nama_lengkap']; ?>" required><br><br>
+        NIP         : <input type="text" name="NIP" value="<?php echo $data2['NIP']; ?>" required><br><br>
+        Jabatan     : <input type="text" name="jabatan" value="<?php echo $data2['jabatan']; ?>" required><br><br>
+        Bidang      : <input type="text" name="bidang" value="<?php echo $data2['bidang']; ?>" required><br><br>
+        Masa Jabatan: <input type="text" name="masa_jabatan" value="<?php echo $data2['masa_jabatan']; ?>" required><br><br>
         
         <button type="submit" name="update">Update Data</button>
     </form>
