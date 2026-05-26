9d669bbac677f07aa33f53286f63bef73d08a57f
AndhiniBaik <andhini.aja09@gmail.com>
2026-05-26 12:11:29 +0700

    koneksi

---
+++ Kesiswaan/koneksi_example.php
@@ -0,0 +1,8 @@
+<?php>
+$koneksi = mysqli_connect(
+"localhost",
+"2526_22", // Sesuaikan jika Anda memiliki username db yang berbeda
+"12345678", // Kosongkan jika menggunakan XAMPP default
+"2526_22db",
+);
+?>
